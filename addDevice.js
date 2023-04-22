e12b0761e6df0ed8f650267a3e2fa9d98bc62cd6
William Hankins <75007804+whankinsiv@users.noreply.github.com>
2023-04-22 13:28:49 -0700

    commit

--- addDevice.js
+++ addDevice.js
@@ -7,3 +7,15 @@ function closeAddDevicePopup() {
     var addDevicePopup = document.getElementById("addDevicePopup");
     addDevicePopup.classList.remove('active');
 }
+
+function submitForm() {
+    var deviceName = document.getElementById("deviceName");
+    console.log(deviceName);
+    var ipAddress = document.getElementById("ipAddress");
+    var userName = document.getElementById("userName");
+    var password = document.getElementById("password");
+    var latitude = document.getElementById("latitude");
+    var longitude = document.getElementById("longitude");
+    var altitude = document.getElementById("altitude");
+
+}
