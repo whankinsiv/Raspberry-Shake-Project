e4e17919473830a03a84d9070aec92ed936e6d36
William Hankins <75007804+whankinsiv@users.noreply.github.com>
2023-04-29 20:15:31 -0700

    Create removeDevice.php

---
+++ removeDevice.php
@@ -0,0 +1,33 @@
+<?php
+$deviceId = $_GET['deviceId'];
+
+$servername = "localhost";
+$database = "u215124131_ERP";
+$username = "u215124131_admin";
+$password = "Password1234";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$query = "DELETE FROM devices WHERE deviceId = '$deviceId'";
+if ($conn->query($query) === TRUE) {
+  echo "Device deleted successfully";
+} else {
+  echo "Error deleting device: " . $conn->error;
+}
+
+// Process the result
+while ($row = $result->fetch_array()) {
+  $data[] = $row;
+}
+echo json_encode($data);
+
+// Close the connection
+mysqli_close($conn);
+
+?>
